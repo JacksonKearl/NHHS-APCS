7759c17b663fc59f6a3c8b8586a897e0689cd56e
Jackson Kearl <jkearl11@gmail.com>
2014-11-21 00:19:25 -0800

    Increased error handeling ability.

--- Java for NHHS APCS/src/morseCodeConversion/MorseDriver.java
+++ Java for NHHS APCS/src/morseCodeConversion/MorseDriver.java
@@ -4,33 +4,44 @@ import javax.swing.JOptionPane;
 
 public class MorseDriver {
 	public static void main(String args[]){
-		Object[] options = { "Alpha to Morse", "Morse to Alpha" };
-		Object selection = JOptionPane.showOptionDialog(null, "What task would you like to perform?", "Task Selection",
-				JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
-				null, options, options[0]);
-		
-		String returnval;
-		if (selection.toString().equals("0")){ 
-			//Alpha to Morse
-			 String alpha = JOptionPane.showInputDialog(null, "What's the sentence?");
+		String optionSelected;
+		do {
+			try{
+				Object[] options = { "Cancel", "Alpha to Morse", "Morse to Alpha" };
+				Object selection = JOptionPane.showOptionDialog(null, "What task would you like to perform?", "Task Selection",
+						JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+						null, options, options[0]);
 
-			 returnval = Morse.toMorse(alpha);
-		}
-		else {                                 
-			//Morse To Alpha
-			 String morse = JOptionPane.showInputDialog(null, "What's the morse?");
-			 morse = Morse.cleanUp(morse);
-			 while (morse == null){
-				 JOptionPane.showMessageDialog(null,
-						    "Input must be only dots, dashes, and spaces.",
-						    "Error",
-						    JOptionPane.ERROR_MESSAGE);
-				 morse = JOptionPane.showInputDialog(null, "Try again. What's the morse?");
-			 }
-			 returnval = Morse.toAlpha(morse);
-		}
-		JOptionPane.showMessageDialog(null,
-			    returnval);
+				String returnval;
+				optionSelected = selection.toString();
 
+				if (selection.toString().equals("1")){ 
+					//Alpha to Morse
+					String alpha = JOptionPane.showInputDialog(null, "What's the sentence?");
+					returnval = Morse.toMorse(alpha);
+					JOptionPane.showMessageDialog(null,
+							returnval);
+
+				}
+				else if (selection.toString().equals("2")){                                 
+					//Morse To Alpha
+					String morse = JOptionPane.showInputDialog(null, "What's the morse?");
+					morse = Morse.cleanUp(morse);
+					while (morse == null){
+						JOptionPane.showMessageDialog(null,
+								"Input must be only dots, dashes, and spaces.",
+								"Error",
+								JOptionPane.ERROR_MESSAGE);
+						morse = JOptionPane.showInputDialog(null, "Try again. What's the morse?");
+					}
+					returnval = Morse.toAlpha(morse);
+					JOptionPane.showMessageDialog(null,
+							returnval);
+				} 
+			} catch (NullPointerException e) { //Called when user hits top left 'x' button.
+				optionSelected = "0";
+			}
+
+		}while (!(optionSelected.equals("0")));
 	}
 }
