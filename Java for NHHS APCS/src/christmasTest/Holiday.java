c4a6cd00340f5f0932e2d4b9f253819c214c31b3
Jackson Kearl <jkearl11@gmail.com>
2014-12-17 00:38:55 -0800

    Entierly recursive

--- Java for NHHS APCS/src/christmasTest/Holiday.java
+++ Java for NHHS APCS/src/christmasTest/Holiday.java
@@ -2,54 +2,56 @@ package christmasTest;
 
 public class Holiday {
 
-	private final int CONSOLE_WIDTH = 80;
-	
-	public Holiday() {	}
-	
+	private final int CONSOLE_WIDTH = 130;
+
+	public Holiday() {}
+
 	public void drawTree(int rows) {
 		drawLeaves(rows);
-		
 		drawBases(rows/4);
-		
 	}
 
 	private void drawLeaves(int currentRow) {
-		if (currentRow == 0){
-			//centerPrint("*");
+		if (currentRow == 0)
 			return;
+		else {
+			drawLeaves(--currentRow);
+			String toPrint = createRow(++currentRow);
+			centerPrint(toPrint);
 		}
-		
-		drawLeaves(--currentRow);
-		String toPrint = createRow(++currentRow);
-		centerPrint(toPrint);
 	}
 
 	private String createRow(int currentRow) {
 		if (currentRow == 1)
 			return "*";
-		return createRow(--currentRow) + "^*";
+		else
+			return createRow(--currentRow) + "^*";
 	}
 
 	private void drawBases(int i) {
-		if (i == 0) return;
-		
-		String toPrint = "MERRY_XMAS";
-		centerPrint(toPrint);
-		
-		drawBases(--i);
-		
+		if (i == 0) 
+			return;
+		else {
+			String toPrint = "MERRY_XMAS";
+			centerPrint(toPrint);
+			drawBases(--i);
+		}
+
 	}
 
 	private void centerPrint(String toPrint) {
-		
 		int centerLine = CONSOLE_WIDTH/2;
 		int halfOffset = toPrint.length()/2;
 		int offsetFromLeft = centerLine-halfOffset;
-		
-		for (int i = 0; i<offsetFromLeft; i++)
-			System.out.print(" ");
-		
-		System.out.println(toPrint);
+
+		System.out.println(gapOf(offsetFromLeft)+toPrint);
+	}
+
+	private String gapOf(int length){
+		if (length == 0) 
+			return "";
+		else
+			return " " + gapOf(--length); 
 	}
 
 }
